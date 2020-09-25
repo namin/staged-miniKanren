831e152b13a1388242c0e0d9611f9f90347853c9
Nada Amin <namin@seas.harvard.edu>
2020-09-25 13:40:43 -0400

    peano number fuzzing

--- fuzzer.scm
+++ fuzzer.scm
@@ -1,16 +1,17 @@
-(gen 'bin-tree? '(t)
-     '(if (number? t)
-	  #t
-	  (and (pair? t)
-	       (pair? (cdr t))
-	       (pair? (cdr (cdr t)))
-	       (pair? (cdr (cdr (cdr t))))
-	      (null? (cdr (cdr (cdr (cdr t)))))
-	      (equal? (car t) 'node)
-	      (number? (car (cdr (cdr t))))
-	      (bin-tree? (car (cdr t)))
-	      (bin-tree? (car (cdr (cdr (cdr t)))))
-	      )))
+;; n = z | s n
+(define nato
+  (eval
+   (gen 'nat? '(x)
+        '(if (equal? 'z x)
+             #t
+             (and (pair? x)
+                  (pair? (cdr x))
+                  (null? (cdr (cdr x)))
+                  (equal? 's (car x))
+                  (nat? (car (cdr x))))))))
+
+(run 10 (q) (nato q #t))
+(run 10 (q) (nato q #f))
 
 (define bin-treeo
 (eval
