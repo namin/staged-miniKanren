6aa6d173962779020bc5113a118a49cb1cbcd7b1
William Byrd <webyrd@gmail.com>
2021-03-07 17:49:08 -0500

    added test

--- tests-bench.scm
+++ tests-bench.scm
@@ -8,6 +8,45 @@
 
 (load "test-check.scm")
 
+(define quasi-quine-evalo
+  (eval
+   (gen 'eval-expr '(expr)
+        `(letrec ([eval-quasi (lambda (q eval)
+                                (match q
+                                  [(? symbol? x) x]
+                                  [`() '()]
+                                  [`(,`unquote ,exp) (eval exp)]
+                                  [`(quasiquote ,datum) 'error]
+                                  [`(,a . ,d)
+                                   (cons (eval-quasi a eval) (eval-quasi d eval))]))]
+                  [eval-expr
+                   (lambda (expr env)
+                     (match expr
+                       [`(quote ,datum) datum]
+                       [`(lambda (,(? symbol? x)) ,body)
+                        (lambda (a)
+                          (eval-expr body (lambda (y)
+                                            (if (equal? x y)
+                                                a
+                                                (env y)))))]
+                       [(? symbol? x) (env x)]
+                       [`(quasiquote ,datum)
+                        (eval-quasi datum (lambda (exp) (eval-expr exp env)))]
+                       [`(,rator ,rand)
+                        ((eval-expr rator env) (eval-expr rand env))]))])
+           (eval-expr expr (lambda (y) 'error))))))
+
+
+(time-test
+  (run 1 (q)
+    (absento 'error q)
+    (absento 'closure q)
+    (quasi-quine-evalo q q))
+  '((((lambda (_.0) `(,_.0 ',_.0)) '(lambda (_.0) `(,_.0 ',_.0)))
+     (=/= ((_.0 closure)) ((_.0 error)))
+     (sym _.0))))
+
+
 (define ho-quine-interp-cons
   (eval
    (gen 'eval-expr '(expr)
