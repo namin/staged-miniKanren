181d9de3cdc909465c3af7ef7d21ebd15048247d
Nada Amin <namin@seas.harvard.edu>
2021-05-24 17:08:04 -0400

    fix macro-time printf meant for runtime

--- staged-run.scm
+++ staged-run.scm
@@ -7,11 +7,12 @@
 (define-syntax run-staged
   (syntax-rules ()
     ((_ n (q) g0 g ...)
-     (printf "running first stage\n")
-     (let* ((f (gen-func (run 100 (q) g0 g ...)))
-            (e (eval f)))
-       (printf "running second stage\n")
-       (run n (q) (e q))))
+     (begin
+       (printf "running first stage\n")
+       (let* ((f (gen-func (run 100 (q) g0 g ...)))
+              (e (eval f)))
+         (printf "running second stage\n")
+         (run n (q) (e q)))))
     ((_ n (q0 q1 q ...) g0 g ...)
      (run-staged n (x)
        (fresh (q0 q1 q ...)
