f39d8dfa5338145ebdbb0dd4a98f577590b848f3
William Byrd <webyrd@gmail.com>
2021-06-29 13:44:12 -0400

    added tests

--- tests-doc.scm
+++ tests-doc.scm
@@ -63,3 +63,35 @@
     ((car '((I love staged evaluation) . _.0)) $$ (absento (call _.0) (closure _.0) (dynamic _.0) (prim _.0)))
     (cons 'I '(love staged evaluation))
     (((lambda _.0 '(I love staged evaluation)) _.1) $$ (=/= ((_.0 quote))) (num _.1) (sym _.0))))
+
+
+
+(define-staged-relation (appendo xs ys zs)
+  (evalo-staged
+   `(letrec ((append
+              (lambda (xs ys)
+                (if (null? xs)
+                    ys
+                    (cons (car xs)
+                          (append (cdr xs) ys))))))
+      (append ',xs ',ys))
+   zs))
+
+(test
+    (run* (q) (appendo '(a b) '(c d e) q))
+    '((a b c d e)))
+
+(test
+    (run* (x y) (appendo x y '(a b c d e)))
+  '(
+    (() (a b c d e))
+    ((a) (b c d e))
+    ((a b) (c d e))
+    ((a b c) (d e))
+    ((a b c d) (e))
+    ((a b c d e) ())
+    ))
+
+(test
+    (run* (x y z) (appendo `(a  . ,x) `(,y e) `(a b c d ,z)))
+  '(((b c) d e)))
