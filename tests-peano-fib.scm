923a512240e222997e001bfdc46db700e9ee13f5
Nada Amin <namin@seas.harvard.edu>
2021-07-01 22:41:08 +0200

    nested letrecs

--- tests-peano-fib.scm
+++ tests-peano-fib.scm
@@ -12,6 +12,89 @@
 (define (peano-fib query)
   `(letrec ((zero?
              (lambda (n)
+               (equal? 'z n))))
+   (letrec ((zero
+             (lambda ()
+               'z)))
+
+   (letrec ((add1
+             (lambda (n)
+               (cons 's n))))
+   (letrec ((sub1
+             (lambda (n)
+               (and (equal? (car n) 's)
+                    (cdr n)))))
+   (letrec ((=
+             (lambda (n m)
+               (if (and (zero? n) (zero? m))
+                   #t
+                   (if (zero? n)
+                       #f
+                       (if (zero? m)
+                           #f
+                           (= (sub1 n) (sub1 m))))))))
+     (letrec ((+
+             (lambda (n m)
+               (if (zero? n)
+                   m
+                   (add1 (+ (sub1 n) m))))))
+      (letrec ((-
+             (lambda (n m)
+               (if (zero? m)
+                   n
+                   (sub1 (- n (sub1 m)))))))
+       (letrec ((*
+             (lambda (n m)
+               (if (zero? n)
+                   (zero)
+                   (+ (* (sub1 n) m) m)))))
+         (letrec ((one
+             (lambda ()
+               (add1 (zero)))))
+         (letrec ((two
+             (lambda ()
+               (add1 (add1 (zero))))))
+          (letrec ((!
+             (lambda (n)
+               (if (zero? n)
+                   (one)
+                   (* n (! (sub1 n)))))))
+          (letrec ((!-aps
+             (lambda (n a)
+               (if (zero? n)
+                   a
+                   (!-aps (sub1 n) (* n a))))))
+            (letrec ((fib-aps
+             (lambda (n a1 a2)
+               (if (zero? n)
+                   a1
+                   (if (zero? (sub1 n))
+                       a2
+                       (fib-aps (- n '(s . z)) a2 (+ a1 a2)))))))
+              ,query
+              ))))))))))))))
+
+(eval (peano-fib `(fib-aps '(s s s s s s . z) 'z '(s . z))))
+
+(test
+    (run-staged #f (v)
+      (evalo-staged
+       (peano-fib `(fib-aps '(s s s s s s . z) 'z '(s . z)))
+       v))
+  '((s s s s s s s s . z)))
+
+(test
+    (run-staged 1 (q)
+      (evalo-staged
+       (peano-fib `(fib-aps ',q 'z '(s . z)))
+       '(s s s s s s s s . z)))
+  '((s s s s s s . z)))
+
+#eof
+
+(define (peano-fib query)
+  `(letrec ((zero?
+             (lambda (n)
                (equal? 'z n)))
             (add1
              (lambda (n)
@@ -74,17 +157,3 @@
      ,query
 
      ))
-
-(test
-    (run-staged #f (v)
-      (evalo-staged
-       (peano-fib `(fib-aps '(s s s s s s . z) 'z '(s . z)))
-       v))
-  '((s s s s s s s s . z)))
-
-(test
-    (run-staged 1 (q)
-      (evalo-staged
-       (peano-fib `(fib-aps ',q 'z '(s . z)))
-       '(s s s s s s s s . z)))
-  '((s s s s s s . z)))
