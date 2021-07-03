c5e0e0ed271236afbb7381aa3fbec1465de2f14f
William Byrd <webyrd@gmail.com>
2021-07-03 11:15:45 -0400

    added staged test

--- tests-dl.scm
+++ tests-dl.scm
@@ -76,6 +76,24 @@
      (absento (call _.0) (closure _.0) (dynamic _.0) (prim _.0)))
     (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top)))))))))))))))))
 
+;; Note the difference in order w.r.t. the unstaged version.
+(record-bench 'run-staged 'nnf-0a)
+(time-test
+  (run-staged 10 (concept)
+    (evalo-staged
+     (nnf concept)
+     '(Not Top)))
+  '((Not Top)
+    (Not (Not (Not Top)))
+    (Not (AtLeast z _.0))
+    (Not (Not (Not (Not (Not Top)))))
+    (Not (Not (Not (AtLeast z _.0))))
+    (Not (Not (Not (Not (Not (Not (Not Top)))))))
+    (Not (Not (Not (Not (Not (AtLeast z _.0))))))
+    (Not (Not (Not (Not (Not (Not (Not (Not (Not Top)))))))))
+    (Not (Not (Not (Not (Not (Not (Not (AtLeast z _.0))))))))
+    (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top)))))))))))))
+
 
 (record-bench 'run-unstaged 'nnf-0b)
 (time-test
@@ -85,7 +103,6 @@
      nnf-concept))
   '((Not Top)))
 
-;; why does the staged version fail, when the unstaged version succeeds?
 (record-bench 'run-staged 'nnf-0b)
 (time-test
   (run-staged #f (nnf-concept)
@@ -119,264 +136,3 @@
     (evalo-staged
      (nnf '(Not (AtLeast (s s s . z) hasChild))) nnf-concept))
   '((AtMost (s s . z) hasChild)))
-
-
-
-(record-bench 'run-unstaged 'nnf-0e)
-(time-test
-  (run* (nnf-concept)
-    (evalo-unstaged
-     `(letrec ((positive?
-                  (lambda (n)
-                    (match n
-                      [`z #f]
-                      [`(s . ,n-1) #t]))))
-          (letrec ((add1
-                    (lambda (n)
-                      (cons 's n))))
-            (letrec ((sub1
-                      (lambda (n)
-                        (match n
-                          [`(s . ,n-1) n-1]))))
-              (letrec ((nnf
-                        (lambda (concept)
-                          (match concept
-                            [`(Not (Not ,c)) (nnf c)]
-                            [`(Not (And ,c1 ,c2))
-                             (list 'Or (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
-                            [`(Not (Or ,c1 ,c2))
-                             (list 'And (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
-                            [`(Not (Exists ,r ,c))
-                             (list 'All r (nnf (list 'Not c)))]
-                            [`(Not (All ,r ,c))
-                             (list 'Exists r (nnf (list 'Not c)))]                          
-                            [`(Not (AtMost ,k ,r))
-                             (list 'AtLeast (add1 k) r)]                          
-                            [`(Not (AtLeast ,k ,r))
-                             (if (positive? k)
-                                 (list 'AtMost (sub1 k) r)
-                                 (list 'Not 'Top))]                    
-                            [`(And ,c1 ,c2)
-                             (list 'And (nnf c1) (nnf c2))]
-                            [`(Or ,c1 ,c2)
-                             (list 'Or (nnf c1) (nnf c2))]
-                            [`(Exists ,r ,c)
-                             (list 'Exists r (nnf c))]
-                            [`(All ,r ,c)
-                             (list 'All r (nnf c))]
-                            [concept concept]))))
-                (nnf '(Not (AtLeast (s s s . z) hasChild)))))))
-     nnf-concept))
-  '((AtMost (s s . z) hasChild)))
-
-
-(record-bench 'run-unstaged 'nnf-0f)
-(time-test
-  (run* (nnf-concept)
-    (evalo-unstaged
-     `(letrec ((nnf
-                (lambda (concept)
-                  (match concept
-                    [`(Not (Not ,c)) (nnf c)]
-                    [`(Not (And ,c1 ,c2))
-                     (list 'Or (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
-                    [`(Not (Or ,c1 ,c2))
-                     (list 'And (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
-                    [`(Not (Exists ,r ,c))
-                     (list 'All r (nnf (list 'Not c)))]
-                    [`(Not (All ,r ,c))
-                     (list 'Exists r (nnf (list 'Not c)))]
-                    #;[`(Not (AtMost ,k ,r)) `(AtLeast ,(+ k 1) ,r)]
-                    #;[`(Not (AtLeast ,k ,r))
-                    (if (> k 0)          ; ;
-                    `(AtMost ,(- k 1) ,r) ; ;
-                    `(Not Top))]
-                    [`(And ,c1 ,c2)
-                     (list 'And (nnf c1) (nnf c2))]
-                    [`(Or ,c1 ,c2)
-                     (list 'Or (nnf c1) (nnf c2))]
-                    [`(Exists ,r ,c)
-                     (list 'Exists r (nnf c))]
-                    [`(All ,r ,c)
-                     (list 'All r (nnf c))]
-                    [concept concept]))))
-        (nnf '(Not (And (Atomic C) (Atomic D)))))
-     nnf-concept))
-  '((Or (Not (Atomic C)) (Not (Atomic D)))))
-
-
-
-(record-bench 'run-unstaged 'nnf-1)
-(time-test
-  (run 10 (concept)
-    (evalo-unstaged
-     `(letrec ((nnf
-                (lambda (concept)
-                  (match concept
-                    [`(Not (Not ,c)) (nnf c)]
-                    [`(Not (And ,c1 ,c2))
-                     (list 'Or (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
-                    #;[`(Not (Or ,c1 ,c2)) `(And ,(nnf `(Not ,c1)) ,(nnf `(Not ,c2)))]
-                    #;[`(Not (Exists ,r ,c)) `(All ,r ,(nnf `(Not ,c)))]
-                    #;[`(Not (All ,r ,c)) `(Exists ,r ,(nnf `(Not ,c)))]
-                    #;[`(Not (AtMost ,k ,r)) `(AtLeast ,(+ k 1) ,r)]
-                    #;[`(Not (AtLeast ,k ,r))
-                    (if (> k 0)          ; ;
-                    `(AtMost ,(- k 1) ,r) ; ;
-                    `(Not Top))]
-                    #;[`(And ,c1 ,c2) `(And ,(nnf c1) ,(nnf c2))]
-                    #;[`(Or ,c1 ,c2) `(Or ,(nnf c1) ,(nnf c2))]
-                    #;[`(Exists ,r ,c) `(Exists ,r ,(nnf c))]
-                    #;[`(All ,r ,c) `(All ,r ,(nnf c))]
-                    [concept concept]))))
-        (nnf ',concept))
-     'Top))
-  '(Top
-    (Not (Not Top))
-    (Not (Not (Not (Not Top))))
-    (Not (Not (Not (Not (Not (Not Top))))))
-    (Not (Not (Not (Not (Not (Not (Not (Not Top))))))))
-    (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top))))))))))
-    (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top))))))))))))
-    (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top))))))))))))))
-    (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top))))))))))))))))
-    (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top))))))))))))))))))))
-
-(record-bench 'run-staged 'nnf-1)
-(time-test
-  (run-staged 10 (concept)
-    (evalo-staged
-     `(letrec ((nnf
-                (lambda (concept)
-                  (match concept
-                    [`(Not (Not ,c)) (nnf c)]
-                    [`(Not (And ,c1 ,c2))
-                     (list 'Or (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
-                    #;[`(Not (Or ,c1 ,c2)) `(And ,(nnf `(Not ,c1)) ,(nnf `(Not ,c2)))]
-                    #;[`(Not (Exists ,r ,c)) `(All ,r ,(nnf `(Not ,c)))]
-                    #;[`(Not (All ,r ,c)) `(Exists ,r ,(nnf `(Not ,c)))]
-                    #;[`(Not (AtMost ,k ,r)) `(AtLeast ,(+ k 1) ,r)]
-                    #;[`(Not (AtLeast ,k ,r))
-                    (if (> k 0)          ; ;
-                    `(AtMost ,(- k 1) ,r) ; ;
-                    `(Not Top))]
-                    #;[`(And ,c1 ,c2) `(And ,(nnf c1) ,(nnf c2))]
-                    #;[`(Or ,c1 ,c2) `(Or ,(nnf c1) ,(nnf c2))]
-                    #;[`(Exists ,r ,c) `(Exists ,r ,(nnf c))]
-                    #;[`(All ,r ,c) `(All ,r ,(nnf c))]
-                    [concept concept]))))
-        (nnf ',concept))
-     'Top))
-  '(Top
-    (Not (Not Top))
-    (Not (Not (Not (Not Top))))
-    (Not (Not (Not (Not (Not (Not Top))))))
-    (Not (Not (Not (Not (Not (Not (Not (Not Top))))))))
-    (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top))))))))))
-    (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top))))))))))))
-    (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top))))))))))))))
-    (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top))))))))))))))))
-    (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top))))))))))))))))))))
-
-
-
-
-(record-bench 'run-unstaged 'nnf-1b)
-(time-test
-  (run* (nnf-concept)
-    (evalo-unstaged
-     `(letrec ((nnf
-                (lambda (concept)
-                  (match concept
-                    [`(Not (Not ,c)) (nnf c)]
-                    [`(Not (And ,c1 ,c2))
-                     (list 'Or (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
-                    #;[`(Not (Or ,c1 ,c2)) `(And ,(nnf `(Not ,c1)) ,(nnf `(Not ,c2)))]
-                    #;[`(Not (Exists ,r ,c)) `(All ,r ,(nnf `(Not ,c)))]
-                    #;[`(Not (All ,r ,c)) `(Exists ,r ,(nnf `(Not ,c)))]
-                    #;[`(Not (AtMost ,k ,r)) `(AtLeast ,(+ k 1) ,r)]
-                    #;[`(Not (AtLeast ,k ,r))
-                    (if (> k 0)          ; ;
-                    `(AtMost ,(- k 1) ,r) ; ;
-                    `(Not Top))]
-                    #;[`(And ,c1 ,c2) `(And ,(nnf c1) ,(nnf c2))]
-                    #;[`(Or ,c1 ,c2) `(Or ,(nnf c1) ,(nnf c2))]
-                    #;[`(Exists ,r ,c) `(Exists ,r ,(nnf c))]
-                    #;[`(All ,r ,c) `(All ,r ,(nnf c))]
-                    [concept concept]))))
-        (nnf 'Top))
-     nnf-concept))
-  '(Top))
-
-(record-bench 'run-staged 'nnf-1b)
-(time-test
-  (run-staged #f (nnf-concept)
-    (evalo-staged
-     `(letrec ((nnf
-                (lambda (concept)
-                  (match concept
-                    [`(Not (Not ,c)) (nnf c)]
-                    [`(Not (And ,c1 ,c2))
-                     (list 'Or (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
-                    #;[`(Not (Or ,c1 ,c2)) `(And ,(nnf `(Not ,c1)) ,(nnf `(Not ,c2)))]
-                    #;[`(Not (Exists ,r ,c)) `(All ,r ,(nnf `(Not ,c)))]
-                    #;[`(Not (All ,r ,c)) `(Exists ,r ,(nnf `(Not ,c)))]
-                    #;[`(Not (AtMost ,k ,r)) `(AtLeast ,(+ k 1) ,r)]
-                    #;[`(Not (AtLeast ,k ,r))
-                    (if (> k 0)          ; ;
-                    `(AtMost ,(- k 1) ,r) ; ;
-                    `(Not Top))]
-                    #;[`(And ,c1 ,c2) `(And ,(nnf c1) ,(nnf c2))]
-                    #;[`(Or ,c1 ,c2) `(Or ,(nnf c1) ,(nnf c2))]
-                    #;[`(Exists ,r ,c) `(Exists ,r ,(nnf c))]
-                    #;[`(All ,r ,c) `(All ,r ,(nnf c))]
-                    [concept concept]))))
-        (nnf 'Top))
-     nnf-concept))
-  '(Top))
-
-
-#!eof
-
-(define 
-  (lambda (concept)
-    (match concept
-      [`(Not (Not ,c)) (nnf c)]
-      [`(Not (And ,c1 ,c2))
-       (list 'Or (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
-      #;[`(Not (Or ,c1 ,c2)) `(And ,(nnf `(Not ,c1)) ,(nnf `(Not ,c2)))]
-      #;[`(Not (Exists ,r ,c)) `(All ,r ,(nnf `(Not ,c)))]
-      #;[`(Not (All ,r ,c)) `(Exists ,r ,(nnf `(Not ,c)))]
-      #;[`(Not (AtMost ,k ,r)) `(AtLeast ,(+ k 1) ,r)]
-      #;[`(Not (AtLeast ,k ,r))
-       (if (> k 0)
-           `(AtMost ,(- k 1) ,r)
-           `(Not Top))]
-      #;[`(And ,c1 ,c2) `(And ,(nnf c1) ,(nnf c2))]
-      #;[`(Or ,c1 ,c2) `(Or ,(nnf c1) ,(nnf c2))]
-      #;[`(Exists ,r ,c) `(Exists ,r ,(nnf c))]
-      #;[`(All ,r ,c) `(All ,r ,(nnf c))]
-      [concept concept])))
-
-
-#!eof
-
-(define nnf
-  (lambda (concept)
-    (match concept
-      [`(Not (Not ,c)) (nnf c)]
-      [`(Not (And ,c1 ,c2)) `(Or ,(nnf `(Not ,c1)) ,(nnf `(Not ,c2)))]
-                            ;; == (list 'Or (nnf (list 'Not c1)) (nnf (list 'Not c2))))
-      [`(Not (Or ,c1 ,c2)) `(And ,(nnf `(Not ,c1)) ,(nnf `(Not ,c2)))]
-      [`(Not (Exists ,r ,c)) `(All ,r ,(nnf `(Not ,c)))]
-      [`(Not (All ,r ,c)) `(Exists ,r ,(nnf `(Not ,c)))]
-      [`(Not (AtMost ,k ,r)) `(AtLeast ,(+ k 1) ,r)]
-      [`(Not (AtLeast ,k ,r))
-       (if (> k 0)
-           `(AtMost ,(- k 1) ,r)
-           `(Not Top))]
-      [`(And ,c1 ,c2) `(And ,(nnf c1) ,(nnf c2))]
-      [`(Or ,c1 ,c2) `(Or ,(nnf c1) ,(nnf c2))]
-      [`(Exists ,r ,c) `(Exists ,r ,(nnf c))]
-      [`(All ,r ,c) `(All ,r ,(nnf c))]
-      [concept concept])))
