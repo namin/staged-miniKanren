01eac9b0c6c858764f92349cebb8692413004125
Nada Amin <namin@seas.harvard.edu>
2021-07-04 12:06:32 +0200

    attempt

--- staged-run.scm
+++ staged-run.scm
@@ -9,7 +9,9 @@
     ((_ n (q) g0 g ...)
      (begin
        (printf "running first stage\n")
-       (let* ((f (gen-func (run 100 (q) g0 g ...)))
+       (let* ((f (gen-func
+                  (parameterize ((staging-time? #t))
+                    (run 100 (q) g0 g ...))))
               (e (eval f)))
          (printf "running second stage\n")
          (run n (q) (e q)))))
@@ -34,5 +36,6 @@
     ((_ (name x0 x ...) g0 g ...)
      (define name
        (eval (gen-func-rel
-              (run 100 (x0 x ...) g0 g ...)
+              (parameterize ((staging-time? #t))
+                (run 100 (x0 x ...) g0 g ...))
               'x0 'x ...))))))
