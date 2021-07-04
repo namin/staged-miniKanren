b2f3707c97f22a21e70bb335aaf49b9b054d8f78
Nada Amin <namin@seas.harvard.edu>
2021-07-04 15:49:29 +0200

    proofo

--- tests-doc.scm
+++ tests-doc.scm
@@ -231,7 +231,43 @@
     ((a b c d e) ())
     ))
 
-;; ###
+;; ### Theorem checker turned prover
+
+(define-staged-relation (proofo proof truth)
+  (evalo-staged
+   `(letrec ([member?
+              (lambda (x ls)
+                (if (null? ls) #f
+                    (if (equal? (car ls) x) #t
+                        (member? x (cdr ls)))))]
+             [proof?
+              (lambda (proof)
+                (match proof
+                  [`(,A ,assms assumption ()) (member? A assms)]
+                  [`(,B ,assms modus-ponens
+                        (((,A => ,B) ,assms ,r1 ,ants1)
+                         (,A ,assms ,r2 ,ants2)))
+                   (and (proof? (list (list A '=> B) assms r1 ants1))
+                        (proof? (list A assms r2 ants2)))]
+                  [`((,A => ,B) ,assms conditional
+                     ((,B (,A . ,assms) ,rule ,ants)))
+                   (proof? (list B (cons A assms) rule ants))]))])
+      (proof? ',proof))
+   truth))
+
+(test
+    (run 1 (prf)
+      (fresh (body)
+        (== prf `(C (A (A => B) (B => C)) . ,body))
+        (proofo prf #t)))
+  '((C (A (A => B) (B => C))
+       modus-ponens
+       (((B => C) (A (A => B) (B => C)) assumption ())
+        (B (A (A => B) (B => C))
+           modus-ponens
+           (((A => B) (A (A => B) (B => C)) assumption ())
+            (A (A (A => B) (B => C)) assumption ())))))))
+
 
 (test
     (run-staged 1 (q)
