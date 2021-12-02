fec8da7273f49b0954005b48b256011849f10dd5
Nada Amin <namin@seas.harvard.edu>
2021-12-02 17:30:35 -0500

    all tests pass

--- tests-staged-apply.scm
+++ tests-staged-apply.scm
@@ -37,49 +37,49 @@
 
 ; the lifted unification from test-rel-staged fails when (5 6 7) is given for res at apply-reified
 (test
-  (run 1 (q)
+  (run-staged 1 (q)
     (fresh (c)
       (reify-call c ((test-rel-staged test-rel-dyn) (2 3) (_ _)))
-      (apply-reified c ((test-rel-staged test-rel-dyn) (_ _) (q '(5 6 7))))))
+      (lapply-reified c ((test-rel-staged test-rel-dyn) (_ _) (q '(5 6 7))))))
   '())
 
 ; the first apply-reified unifies the shared / first-stage argument v with 3, so q gets 3.
 (test
-  (run 1 (q)
+  (run-staged 1 (q)
     (fresh (c v)
       (reify-call c ((test-rel-staged test-rel-dyn) (2 v) (_ _)))
-      (apply-reified c ((test-rel-staged test-rel-dyn) (_ _) (1 '(1 2 3))))
-      (apply-reified c ((test-rel-staged test-rel-dyn) (_ _) (4 `(4 2 ,q))))))
+      (lapply-reified c ((test-rel-staged test-rel-dyn) (_ _) (1 '(1 2 3))))
+      (lapply-reified c ((test-rel-staged test-rel-dyn) (_ _) (4 `(4 2 ,q))))))
   '(3))
 
 ; fails because the two apply-reifieds try to unify v with different values
 (test
-  (run 1 (q)
+  (run-staged 1 (q)
     (fresh (c v)
       (reify-call c ((test-rel-staged test-rel-dyn) (2 v) (_ _)))
-      (apply-reified c ((test-rel-staged test-rel-dyn) (_ _) (1 '(1 2 3))))
-      (apply-reified c ((test-rel-staged test-rel-dyn) (_ _) (4 '(4 2 5))))))
+      (lapply-reified c ((test-rel-staged test-rel-dyn) (_ _) (1 '(1 2 3))))
+      (lapply-reified c ((test-rel-staged test-rel-dyn) (_ _) (4 '(4 2 5))))))
   '())
 
 ; uses test-rel-dyn to run backwards; no staging, a and b args are unified with 1 and 2 at reify-call
 (test
-  (run 1 (q)
+  (run-staged 1 (q)
     (fresh (c)
-      (apply-reified c ((test-rel-staged test-rel-dyn) (_ _) (1 q)))
+      (lapply-reified c ((test-rel-staged test-rel-dyn) (_ _) (1 q)))
       (reify-call c ((test-rel-staged test-rel-dyn) (2 3) (_ _)))))
   '((1 2 3)))
 
 ; fails when arg unification fails at reify-call
 (test
-  (run 1 (q)
+  (run-staged 1 (q)
     (fresh (c)
-      (apply-reified c ((test-rel-staged test-rel-dyn) (_ _) (1 '(1 2 3))))
+      (lapply-reified c ((test-rel-staged test-rel-dyn) (_ _) (1 '(1 2 3))))
       (reify-call c ((test-rel-staged test-rel-dyn) (5 6) (_ _)))))
   '())
 
 ; fails when to reified calls with ununifiable arguments are unified
 (test
-  (run 1 (q)
+  (run-staged 1 (q)
     (fresh (c)
       (reify-call c ((test-rel-staged test-rel-dyn) (1 2) (_ _)))
       (reify-call c ((test-rel-staged test-rel-dyn) (3 4) (_ _)))))
@@ -88,8 +88,8 @@
 ; fails because the dynamic evaluation in the two apply-reifieds force the (unseen)
 ; first-stage argument y to different values
 (test
-  (run 1 (q)
+  (run-staged 1 (q)
     (fresh (c v)
-      (apply-reified c ((test-rel-staged test-rel-dyn) (_ _) (1 '(1 2 3))))
-      (apply-reified c ((test-rel-staged test-rel-dyn) (_ _) (4 '(4 2 5))))))
+      (lapply-reified c ((test-rel-staged test-rel-dyn) (_ _) (1 '(1 2 3))))
+      (lapply-reified c ((test-rel-staged test-rel-dyn) (_ _) (4 '(4 2 5))))))
   '())
