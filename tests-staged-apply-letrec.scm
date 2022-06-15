40623a7bc54da8a965fb945937d1f4053ed6b679
Nada Amin <namin@seas.harvard.edu>
2022-06-14 20:14:52 -0400

    letrec^ handling all sorts of args

--- tests-staged-apply-letrec.scm
+++ tests-staged-apply-letrec.scm
@@ -17,6 +17,20 @@
 (test
     (run-staged 1 (q)
       (evalo-staged
+       `(letrec^ ((f (lambda x (car x)))) (f 1))
+       q))
+  '(1))
+
+(test
+    (run-staged 1 (q)
+      (evalo-staged
        `(letrec^ ((f (lambda (x) (if (pair? x) (f (car x)) x)))) (f '(1 2)))
+       q))
+  '(1))
+
+(test
+    (run-staged 1 (q)
+      (evalo-staged
+       `(letrec^ ((f (lambda x (if (pair? (car x)) (f (car (car x))) (car x))))) (f '(1 2)))
        q))
   '(1))
