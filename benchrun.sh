d20b7119d1c1ccc0b62b082b753f6a1edb6ada65
Nada Amin <namin@seas.harvard.edu>
2022-06-24 15:10:23 -0400

    typo

--- benchrun.sh
+++ benchrun.sh
@@ -13,6 +13,6 @@ python benchread.py >../staged-mk-paper/bench_doc.tex
 cp bench-log-ex.txt bench-log-ex-doc.txt
 
 #chez --script tests-double-eval.scm | tee bench-log-ex.txt
-racket tests-double-eval-racket.rkt | tee bench log-ex.txt
+racket tests-double-eval-racket.rkt | tee bench-log-ex.txt
 python benchread.py >../staged-mk-paper/bench_double_eval.tex
 cp bench-log-ex.txt bench-log-ex-double-eval.txt
