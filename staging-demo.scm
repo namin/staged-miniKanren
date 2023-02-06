f3dc0f8bbc404b01402dfefb3dc4582aa81b1e54
Nada Amin <namin@seas.harvard.edu>
2023-02-06 15:56:28 -0500

    staging demo

---
+++ staging-demo.scm
@@ -0,0 +1,51 @@
+(run* (q)
+  (l== q 1))
+
+(run-staged* (q)
+  (l== q 1))
+res
+
+(run-staged* (q)
+  (conde
+    ((l== q 1))
+    ((l== q 2))))
+
+(run-staged* (q)
+  (condg
+   (l== q 3)
+   ([] [(== q 1)] [(l== q 1)])
+   ([] [(== q 2)] [(l== q 2)])))
+
+(run-staged* (q)
+  (== q 1)
+  (condg
+   (l== q 3)
+   ([] [(== q 1)] [(l== q 1)])
+   ([] [(== q 2)] [(l== q 2)])))
+
+(define (u-minio expr val)
+  (conde
+    ((numbero expr)
+     (== expr val))
+    ((symbolo expr)
+     (== 'SYM val))
+    ((fresh (e1 e2 v1 v2)
+       (== `(,e1 ,e2) expr)
+       (== `(,v1 ,v2) val)
+       (u-minio e1 v1)
+       (u-minio e2 v2)))))
+
+(run* (q) (u-minio '(hello 1) q))
+
+(define (minio expr val)
+  (condg
+   (u-minio expr val)
+   ([] [(numbero expr)] [(l== expr val)])
+   ([] [(symbolo expr)] [(l== 'SYM val)])
+   ([e1 e2 v1 v2] [(== `(,e1 ,e2) expr)]
+    [(l== `(,v1 ,v2) val)
+     (u-minio e1 v1)
+     (u-minio e2 v2)])))
+
+(run-staged* (q) (minio '(hello 1) q))
+res
