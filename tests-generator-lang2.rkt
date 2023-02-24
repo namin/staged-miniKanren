44a0c65cf57a16edd7e4597b3d59f6607d4e1f56
Michael Ballantyne <michael.ballantyne@gmail.com>
2023-02-23 20:57:57 -0500

    add some failing tests re now in  later conde

--- tests-generator-lang2.rkt
+++ tests-generator-lang2.rkt
@@ -26,3 +26,62 @@
  (run 1 (q)
    (bar q))
  '(1))
+
+
+(test
+ (run 1 (q)
+   (staged
+    (fresh (x)
+      (== x 1)
+      (conde
+       [(== x 1)
+        (later (== q 1))]
+       [(== x 2)
+        (later (== q 2))]))))
+ '(1))
+
+(test
+ (run 2 (q)
+   (staged
+    (fresh (x y)
+      (later
+       (fresh ()
+         (== x q)
+         (conde
+          [(now (symbolo x)) (== y 1)]
+          [(now (numbero x)) (== y 1)]))))))
+ '((_.0 $$ (sym _.0)) (_.0 $$ (num _.0))))
+
+;; TODO: this generates code for the symbolo and numbero even though they aren't for a variable that's relevant at runtime
+(test
+ (run 2 (q)
+   (staged
+    (fresh (x y)
+      (later
+       (fresh ()
+         (conde
+          [(now (symbolo x)) (== y 1)]
+          [(now (numbero x)) (== y 1)]))))))
+ '(_.0 _.0))
+
+
+;; TODO: should be okay if there are no `later`s in a staged
+(test
+ (run 1 (q)
+   (staged
+    (== q 1)))
+ '(1))
+
+;; TODO: this fails for two reasons.
+;; - conde: bad syntax in: (conde () ())
+;; - we don't capture substitution extensions in conde
+(test
+ (run 2 (q)
+   (staged
+    (fresh (x y)
+      (== q x)
+      (later
+       (conde
+        [(now (== x 1))]
+        [(now (== x 2))])))))
+ '(1 2))
