adeac6480b0f8448b8fb2d80e4f594705d473d7d
Nada Amin <namin@seas.harvard.edu>
2023-03-24 11:29:01 -0400

    dead code elim

--- staged-interp.rkt
+++ staged-interp.rkt
@@ -8,13 +8,6 @@
  u-eval-expo)
 
 (require "generator-lang.rkt"
-         
-         ;; things that really shouldn't be used in enerator code, but we haven't finished
-         ;; transforming the interpreter to use condg yet
-         (only-in "staged-load.rkt"
-                  varo
-                  non-varo)
-         
          racket/include)
 
 (include "unstaged-interp.scm")
