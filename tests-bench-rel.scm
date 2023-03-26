76f610cee2e051ff709c8a3c32af52d0c8df5cb1
Nada Amin <namin@seas.harvard.edu>
2023-03-26 12:34:20 -0400

    wip: fails

--- tests-bench-rel.scm
+++ tests-bench-rel.scm
@@ -1,4 +1,4 @@
+(load "tests-dl.scm") ;; TODO: reorder after tests-proof.scm
 (load "tests-parsing-with-derivatives.scm")
 (load "tests-proof.scm")
-(load "tests-dl.scm")
 (load "tests-peano-fib.scm")
--- tests-dl.scm
+++ tests-dl.scm
@@ -1,80 +1,87 @@
 ;; Adapted from the nnf code in 'The Semantic Web Explained' by Péter
 ;; Szeredi, Gergely Lukácsy, and Tamás Benkő. Cambridge University
 ;; Press, 2014.
-(define nnf
-  (lambda (concept)
-    `(letrec ((positive?
+(define-term-syntax-rule (nnf concept)
+  `(letrec ((positive?
+             (lambda (n)
+               (match n
+                 [`z #f]
+                 [`(s . ,n-1) #t]))))
+     (letrec ((add1
                (lambda (n)
-                 (match n
-                   [`z #f]
-                   [`(s . ,n-1) #t]))))
-       (letrec ((add1
+                 (cons 's n))))
+       (letrec ((sub1
                  (lambda (n)
-                   (cons 's n))))
-         (letrec ((sub1
-                   (lambda (n)
-                     (match n
-                       [`(s . ,n-1) n-1]))))
-           (letrec ((nnf
-                     (lambda (concept)
-                       (match concept
-                         [`(Not (Not ,c)) (nnf c)]
-                         [`(Not (And ,c1 ,c2))
-                          (list 'Or (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
-                         [`(Not (Or ,c1 ,c2))
-                          (list 'And (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
-                         [`(Not (Exists ,(? symbol? r) ,c))
-                          (list 'All r (nnf (list 'Not c)))]
-                         [`(Not (All ,(? symbol? r) ,c))
-                          (list 'Exists r (nnf (list 'Not c)))]
-                         [`(Not (AtMost ,k ,(? symbol? r)))
-                          (list 'AtLeast (add1 k) r)]
-                         [`(Not (AtLeast ,k ,(? symbol? r)))
-                          (if (positive? k)
-                              (list 'AtMost (sub1 k) r)
-                              (list 'Not 'Top))]    
-                         [`(And ,c1 ,c2)
-                          (list 'And (nnf c1) (nnf c2))]
-                         [`(Or ,c1 ,c2)
-                          (list 'Or (nnf c1) (nnf c2))]
-                         [`(Exists ,(? symbol? r) ,c)
-                          (list 'Exists r (nnf c))]
-                         [`(All ,(? symbol? r) ,c)
-                          (list 'All r (nnf c))]
-                         [concept concept]))))
-             (nnf ',concept)))))))
+                   (match n
+                     [`(s . ,n-1) n-1]))))
+         (letrec ((nnf
+                   (lambda (concept)
+                     (match concept
+                       [`(Not (Not ,c)) (nnf c)]
+                       [`(Not (And ,c1 ,c2))
+                        (list 'Or (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
+                       [`(Not (Or ,c1 ,c2))
+                        (list 'And (nnf (list 'Not c1)) (nnf (list 'Not c2)))]
+                       [`(Not (Exists ,(? symbol? r) ,c))
+                        (list 'All r (nnf (list 'Not c)))]
+                       [`(Not (All ,(? symbol? r) ,c))
+                        (list 'Exists r (nnf (list 'Not c)))]
+                       [`(Not (AtMost ,k ,(? symbol? r)))
+                        (list 'AtLeast (add1 k) r)]
+                       [`(Not (AtLeast ,k ,(? symbol? r)))
+                        (if (positive? k)
+                            (list 'AtMost (sub1 k) r)
+                            (list 'Not 'Top))]    
+                       [`(And ,c1 ,c2)
+                        (list 'And (nnf c1) (nnf c2))]
+                       [`(Or ,c1 ,c2)
+                        (list 'Or (nnf c1) (nnf c2))]
+                       [`(Exists ,(? symbol? r) ,c)
+                        (list 'Exists r (nnf c))]
+                       [`(All ,(? symbol? r) ,c)
+                        (list 'All r (nnf c))]
+                       [concept concept]))))
+           (nnf ',concept))))))
 
 (record-bench 'staging 'nnf)
-(define-staged-relation (nnfo concept nnf-concept)
-  (evalo-staged
-   (nnf concept)
-   nnf-concept))
+(defrel (nnfo concept nnf-concept)
+  (staged
+   (evalo-staged
+    (nnf concept)
+    nnf-concept)))
+
+(define nnfo-10-results
+  `((Not Top)
+  ((Not (AtLeast z _.0))
+    $$
+    ,not-tags0
+    (sym _.0))
+  (Not (Not (Not Top)))
+  ((Not (Not (Not (AtLeast z _.0))))
+    $$
+    ,not-tags0
+    (sym _.0))
+  (Not (Not (Not (Not (Not Top)))))
+  ((Not (Not (Not (Not (Not (AtLeast z _.0))))))
+    $$
+    ,not-tags0
+    (sym _.0))
+  (Not (Not (Not (Not (Not (Not (Not Top)))))))
+  ((Not (Not (Not (Not (Not (Not (Not (AtLeast z _.0))))))))
+    $$
+    ,not-tags0
+    (sym _.0))
+  (Not (Not (Not (Not (Not (Not (Not (Not (Not Top)))))))))
+  ((Not (Not (Not (Not (Not (Not (Not (Not (Not (AtLeast z _.0))))))))))
+   $$
+   ,not-tags0
+   (sym _.0))))
 
 (record-bench 'staged 'nnf 0)
 (time-test
   (run 10 (concept)
     (nnfo concept '(Not Top)))
-  `((Not Top) (Not (Not (Not Top)))
-  ((Not (AtLeast z _.0))
-    $$
-    ,not-tags0
-    (sym _.0))
-  (Not (Not (Not (Not (Not Top)))))
-  ((Not (Not (Not (AtLeast z _.0))))
-    $$
-    ,not-tags0
-    (sym _.0))
-  (Not (Not (Not (Not (Not (Not (Not Top)))))))
-  ((Not (Not (Not (Not (Not (AtLeast z _.0))))))
-    $$
-    ,not-tags0
-    (sym _.0))
-  (Not (Not (Not (Not (Not (Not (Not (Not (Not Top)))))))))
-  ((Not (Not (Not (Not (Not (Not (Not (AtLeast z _.0))))))))
-    $$
-    ,not-tags0
-    (sym _.0))
-  (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not (Not Top)))))))))))))
+  nnfo-10-results)
 
 (record-bench 'unstaged 'nnf 0)
 (time-test
@@ -85,15 +92,14 @@
      '(Not Top))))
  10)
 
-;; Note the difference in order w.r.t. the unstaged version.
 (record-bench 'run-staged 'nnf 0)
 (time-test
- (length
-  (run-staged 10 (concept)
+ (run 10 (concept)
+   (staged
     (evalo-staged
      (nnf concept)
      '(Not Top))))
- 10)
+ nnfo-10-results)
 
 
 
