eb0fc82720b507f532a1f45fe03e64cf21d2bd63
Michael Ballantyne <michael.ballantyne@gmail.com>
2023-04-28 16:01:18 -0400

    benchrun: new paths, python3

--- benchrun.sh
+++ benchrun.sh
@@ -2,17 +2,14 @@
 
 set -e
 
-#chez --script tests-bench-rel.scm | tee bench-log-ex.txt
-racket tests-bench-rel-racket.rkt | tee bench-log-ex.txt
-python benchread.py >../staged-mk-paper/bench_rel.tex
+racket tests-bench-rel.rkt | tee bench-log-ex.txt
+python3 benchread.py >../staged-mk-paper/bench_rel.tex
 cp bench-log-ex.txt bench-log-ex-rel.txt
 
-#chez --script tests-doc-bench.scm | tee bench-log-ex.txt
-racket tests-doc-bench-racket.rkt | tee bench-log-ex.txt
-python benchread.py >../staged-mk-paper/bench_doc.tex
+racket tests-doc-bench.rkt | tee bench-log-ex.txt
+python3 benchread.py >../staged-mk-paper/bench_doc.tex
 cp bench-log-ex.txt bench-log-ex-doc.txt
 
-#chez --script tests-double-eval.scm | tee bench-log-ex.txt
-racket tests-double-eval-racket.rkt | tee bench-log-ex.txt
-python benchread.py >../staged-mk-paper/bench_double_eval.tex
+racket tests-double-eval.rkt | tee bench-log-ex.txt
+python3 benchread.py >../staged-mk-paper/bench_double_eval.tex
 cp bench-log-ex.txt bench-log-ex-double-eval.txt
