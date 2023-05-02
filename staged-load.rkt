3aa97620434159f3125dfef0ba03b2e80d1daac5
Michael Ballantyne <michael.ballantyne@gmail.com>
2023-05-02 17:38:49 -0400

    remove old helpers

--- staged-load.rkt
+++ staged-load.rkt
@@ -17,33 +17,3 @@
 (include "staged-utils.scm")
 (include "staged-run.scm")
 (include "test-check.scm")
-
-(define (mapo fo xs ys)
-  (conde
-    ((== xs '()) (== ys '()))
-    ((fresh (xa xd ya yd)
-       (== xs (cons xa xd))
-       (== ys (cons ya yd))
-       (fo xa ya)
-       (mapo fo xd yd)))))
-
-(define (make-list-of-symso xs ys)
-  (mapo (lambda (x y) (== y (unexpand x))) xs ys))
-
-(define (varo x)
-  (lambda (c)
-    (if (var? (walk* x (state-S c)))
-        c
-        #f)))
-
-(define (non-varo x)
-  (lambda (c)
-    (if (var? (walk* x (state-S c)))
-        #f
-        c)))
-
-(define (logo f . args)
-  (lambda (c)
-    (apply printf f (walk* args (state-S c)))
-    (newline)
-    c))
