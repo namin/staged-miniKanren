2e052e8e00c9c1f7431d7193c7db1c96b418c4fa
Michael Ballantyne <michael.ballantyne@gmail.com>
2023-05-15 18:16:49 -0400

    explain the fallback search more via the tests

--- tests/language/fallback.rkt
+++ tests/language/fallback.rkt
@@ -68,36 +68,17 @@
 ;; back. But I think that would lead to diverging searches at staging time when you
 ;; want to defer the divergence to runtime.
 
-;; Recognizing the need to fall back here is made tricky by the recursion nesting
-;; fallback forms. For some fallback to fall back, it has to know that both its base
-;; and recursive cases can succeed. But its recursive case contains a nested fallback
-;; form with the same challenge! We can only successfully fall back if the inner fallback
-;; reports out that it has a base-case branch that can succeed, thus allowing the outer
-;; fallback form to know it has at least two solutions.
-;;
-;; Also notice that we still need interleaving search here! A depth-first-search would
-;; just keep going deeper into the recursive case, because it is ordered first. It
-;; would never discover the base-case successes that allow us to fall back.
-(defrel/generator (membero x l log)
-  (fallback
-   (later (== log 'fallback))
-   (fresh (first rest log-rest)
-     (== `(,first . ,rest) l)
-     (conde
-       [(=/= x first)
-        (== log `(recursive-case . ,log-rest))
-        (membero x rest log-rest)]
-       [(== x first)
-        (== log 'base-case)]))))
-(test
- (run 1 (q)
-   (staged
-    (fresh (l)
-      (membero 'x `(y . ,l) q))))
- '((recursive-case . fallback)))
 
-;; In this example the possibilities of q = 1 and q = 2 should be enough to
-;; trigger the outer fallback, even though the `nevero` never terminates.
+;; The discovery of two possible answers anywhere under a fallback triggers
+;; use of the fallback goal even when there are other branches that expand indefinitely.
+;;
+;; In this example the success of branch-1 and branch-2
+;; triggers the outer fallback, even though the `nevero` never terminates.
+;;
+;; Note that this design means that the implementation of fallback cannot simply run*
+;; all the branches to determine if there is nondeterminism! With that strategy, the
+;; inner fallback form would fail to terminate. Instead, the computation terminates when
+;; the outer fallback finds out about the success of branch-2 and triggers fallback-1.
 (defrel/generator (nevero)
   (conde
     [fail]
@@ -115,11 +96,48 @@
            [(== q 'branch-2)]
            [(nevero)]))]))))
  '(fallback-1))
+;;
+;; To see why the above behavior matters in practice, consider `membero`:
+(defrel/generator (membero x l log)
+  (fallback
+   (later (== log 'fallback))
+   (fresh (first rest log-rest)
+     (== `(,first . ,rest) l)
+     (conde
+       [(=/= x first)
+        (== log `(recursive-case . ,log-rest))
+        (membero x rest log-rest)]
+       [(== x first)
+        (== log 'base-case)]))))
+;; And in particular, the situation where the list is unknown:
+(test
+ (run 1 (q)
+   (staged
+    (fresh (l)
+      (membero 'x l q))))
+ '(fallback))
+;; This is analogous to situations in the staged interpreter where the expression
+;; is unknown.
+;;
+;; We want fallback to happen in this example. To achieve that, evaluation has
+;; to determine that there are multiple answers, in particular l = (x . _.0) and
+;; l = (_.0 x . _.1) w/ (=/= x _.0).
+;;
+;; But the outer fallback must not run* its second branch, because the recursive
+;; case can expand to arbitrary depth! The outer fallback must come to know about
+;; the l = (_.0 x . _.1) w/ (=/= 'x _.0) answer from its recursive case without
+;; forcing evaluation of the rest of the recursive case.
+;;
+;; This is the first of two constraints that make the implementation tricky.
 
-;; Here we must run the (== q 2) goal outside of the inner fallback in order to
-;; discover that the outer fallback is nondeterministic. This means that the first
-;; solution of the inner fallback must be tried with the success-k of the inner
-;; fallback before we have committed to the decision of whether to fall back or not.
+
+;; In general it is not enough to simply know that an inner fallback has an answer.
+;; Determining whether an outer fallback should trigger may require running further
+;; goals in a conjunction with the result of an inner fallback.
+;;
+;; Here, we must run the (== x 2) goal outside of the inner fallback in order to
+;; discover that the answer from the inner fallback actually leads the outer fallback
+;; to be nondeterministic:
 (test
  (run 1 (q)
    (staged
@@ -135,9 +153,35 @@
              [(nevero)]))
           (== x 2))]))))
  '(fallback-1))
+;; This means that the first solution of the inner fallback must be tried with the
+;; success-k of the inner fallback before we have committed to the decision of whether
+;; to fall back or not.
+;;
+;; We must interleave evaluation of (a) the further conjuncts based on the inner fallback's
+;; with (b) attempting to find additional solutions to the inner fallback's body:
+;;   - (a) may never succeed, in which case the only way to succeed overall is to discover
+;;    more solutions from (b) to trigger the inner fallback
+;;   - (b) may never succeed, in which case (a) is the only way to succeed overall.
+;;
+;; But in the case that (b) ever succeeds, our semantics demands that we return the fallback
+;; result and not the result of (a)! So when (b) succeeds, we have to claw back and cancel
+;; the work on (a).
+;;
+;; This is the second constraint that makes the implementation challenging.
 
-;; Regression test: success of the first goal in a conjunct must not notify
-;; success of the whole conjunction.
+
+;; To simultaneously solve the two constraints, the implementation must *notify* surrounding
+;; fallbacks that a solution has been reached while still retaining control over the solution
+;; in order to be able to replace it with the fallback if another solution is reached.
+;;
+;; Outer fallback forms count the notifications, and fallback if they receive more than one
+;; such notification.
+
+
+;; The remaining tests relate to the way these notifications propagate through other
+;; kinds of goals.
+
+;; Success of the first goal in a conjunct must not notify uccess of the whole conjunction.
 ;;
 ;; In this example the first goal (== 1 1) in the first branch will
 ;; succeed, but the overall branch will fail. We don't want to notify-success
@@ -165,10 +209,12 @@
             ((== 1 1) (== x 1) (later (== q 'branch-1)))
             ((== x 2) (later (== q 'branch-2))))))))
  '(fallback))
+;;
+;; See the use of `ss:tag-notify` and `ss:filter-notify` in `ss:conj2` in the implementation.
 
-;; Regression test: notify-success while evaluating a conjunct outside of and after
-;; a fallback (that is, in the success continuation given to the fallback) should not be
-;; counted by the fallback as indicating nondeterminism.
+;; notify-success while evaluating a conjunct outside of and after a fallback (that is,
+;; in the success continuation given to the fallback) should not be counted by the fallback
+;; as indicating nondeterminism.
 (test
  (run 1 (x)
       (staged
@@ -178,9 +224,12 @@
           (later (== x 'only-answer)))
          (== 1 1))))
  '(only-answer))
+;; However, the notification must propagate out to trigger outer fallbacks.
+;;
+;; See success-k^ and the (set-member? tags ignore-tag) checks inside ss:maybe-fallback.
 
 
-;; Regression test: success of the fallback goal should not cause an outer
+;; Success of the fallback goal should not cause an outer
 ;; fallback form to double-count.
 ;;
 ;; Here both branches of the inner disjunction will succeed. The first such
@@ -210,7 +259,6 @@
     (fresh (y)
       (r 2 y outer inner))))
  '((outer-commit-2 inner-fallback)))
-
 ;; However, a second success from the fallback goal *should* be notified to the
 ;; surrounding fallback form, leading it to fall back.
 (defrel/generator (r2 x y outer inner)
@@ -236,10 +284,12 @@
     (fresh (y)
       (r2 2 y outer inner))))
  '((outer-fallback _.0)))
+;; See ss:drop-one-notify and its use in ss:maybe-fallback in the implementation.
 
 
-;; Regression test: partial-apply needs to notify success to trigger fallback of
-;; surrounding branches; initially it did not. Right now, it uses ss:promise-success
+
+;; partial-apply needs to notify success to trigger fallback of surrounding branches.
+;; Right now, it uses ss:promise-success
 ;; to notify before even running the partial's generator, and unique-result to require
 ;; that generator to succeed.  TODO: in the future I would like to make it okay for the
 ;; generator to fail.
@@ -260,7 +310,7 @@
        ((later (== q 1)))))))
  '(3))
 
-;; Regresssion test: similarly, later conde needs to notify. Currently it does this by
+;; Similarly, later conde needs to notify. Currently it does this by
 ;; promising and requring success of everything within, but TODO: I'd like to change this
 ;; to only succeed if the conjunction generating all branches succeeds.
 (test
