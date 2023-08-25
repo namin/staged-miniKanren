44aac6914460e01ce5149a80ee228361f1c1e251
Nada Amin <namin@seas.harvard.edu>
2023-08-25 15:05:49 +0200

    infer from faster-mk

---
+++ tests/infer.rkt
@@ -0,0 +1,50 @@
+#lang racket/base
+
+(require racket/pretty
+         "../main.rkt"
+         "../test-check.rkt")
+
+(defrel/multistage/fallback (!- exp env t)
+  (conde
+    [(fresh (env-t)
+       (symbolo exp)
+       (later (== t env-t))
+       (lookupo exp env env-t))]
+    [(fresh (x e t-x t-e)
+       (== `(lambda (,x) ,e) exp)
+       (symbolo x)
+       (not-in-envo 'lambda env)
+       (later (== `(-> ,t-x ,t-e) t))
+       (!- e `((,x . ,t-x) . ,env) t-e))]
+    [(fresh (rator rand t-x)
+       (== `(,rator ,rand) exp)
+       (!- rator env `(-> ,t-x ,t))
+       (!- rand env t-x))]))
+
+(defrel/multistage/fallback (lookupo x env t)
+  (fresh (rest y v)
+    (== `((,y . ,v) . ,rest) env)
+    (conde
+      ((== y x) (== v t))
+      ((=/= y x) (lookupo x rest t)))))
+
+(defrel/multistage/fallback (not-in-envo x env)
+  (conde
+    ((== '() env))
+    ((fresh (y v rest)
+       (== `((,y . ,v) . ,rest) env)
+       (=/= y x)
+       (not-in-envo x rest)))))
+
+(test
+  (run 1 (t) (staged (!- '(lambda (x) (lambda (f) (f x))) '() t)))
+  '((-> _.0 (-> (-> _.0 _.1) _.1))))
+
+(test
+  (run 3 (e t) (staged (!- `(lambda (x) (lambda (f) (f ,e))) '() t)))
+  '((x (-> _.0 (-> (-> _.0 _.1) _.1)))
+    (((lambda (_.0) _.0) (-> _.1 (-> (-> (-> _.2 _.2) _.3) _.3))) $$ (sym _.0))
+    (((lambda (_.0) x) (-> _.1 (-> (-> (-> _.2 _.1) _.3) _.3)))
+     $$
+     (=/= ((_.0 x)))
+     (sym _.0))))
