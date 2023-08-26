641a47ffab4821f0036785f99628804a99685a3e
Nada Amin <namin@seas.harvard.edu>
2023-08-26 14:49:40 +0200

    turn into tests

--- tests/applications/metaKanren.rkt
+++ tests/applications/metaKanren.rkt
@@ -493,39 +493,43 @@ Syntax
 
 ;; tests
 
-(run* (x)
-  (eval-programo
-    `(run* (z)
-       (letrec-rel ((appendo (l1 l2 l)
-                      (disj
-                        (conj (== '() l1) (== l2 l))
-                        (fresh (a)
-                          (fresh (d)
-                            (fresh (l3)
-                              (conj (== (cons a d) l1)
-                                    (conj (== (cons a l3) l)
-                                          (delay (call-rel appendo d
-                                                                   l2
-                                                                   l3))))))))))
-          (call-rel appendo '(1 2) '(3 4) z)))
-    x))
+(test
+  (run* (x)
+    (eval-programo
+     `(run* (z)
+        (letrec-rel ((appendo (l1 l2 l)
+                              (disj
+                               (conj (== '() l1) (== l2 l))
+                               (fresh (a)
+                                 (fresh (d)
+                                   (fresh (l3)
+                                     (conj (== (cons a d) l1)
+                                           (conj (== (cons a l3) l)
+                                                 (delay (call-rel appendo d
+                                                                  l2
+                                                                  l3))))))))))
+                    (call-rel appendo '(1 2) '(3 4) z)))
+     x))
+  '(((1 2 3 4))))
 
-(run* (x)
-  (eval-programo
-    `(run* (z)
-       (letrec-rel ((appendo (l1 l2 l)
-                      (disj
-                        (conj (== '() l1) (== l2 l))
-                        (fresh (a)
-                          (fresh (d)
-                            (fresh (l3)
-                              (,x (== (cons a d) l1)
-                                  (conj (== (cons a l3) l)
-                                        (delay (call-rel appendo d
-                                                                 l2
-                                                                 l3))))))))))
-          (call-rel appendo '(1 2) '(3 4) '(1 2 3 4))))
-    '((_.))))
+(test
+  (run* (x)
+    (eval-programo
+     `(run* (z)
+        (letrec-rel ((appendo (l1 l2 l)
+                              (disj
+                               (conj (== '() l1) (== l2 l))
+                               (fresh (a)
+                                 (fresh (d)
+                                   (fresh (l3)
+                                     (,x (== (cons a d) l1)
+                                         (conj (== (cons a l3) l)
+                                               (delay (call-rel appendo d
+                                                                l2
+                                                                l3))))))))))
+                    (call-rel appendo '(1 2) '(3 4) '(1 2 3 4))))
+     '((_.))))
+  '(conj))
 
 ; Gives disj in addition to conj
 ;; TODO
@@ -547,82 +551,98 @@ Syntax
           (call-rel appendo '(1 2) '(3 4) '(1 2 3 4))))
     '((_.))))
 
-(run 1 (x)
-  (eval-programo
-    `(run* (z)
-       (letrec-rel ((five (f)
-                      (== 5 f)))
-          (call-rel five z)))
-    x))
+(test
+  (run 1 (x)
+    (eval-programo
+     `(run* (z)
+        (letrec-rel ((five (f)
+                           (== 5 f)))
+                    (call-rel five z)))
+     x))
+  '((5)))
 
 ; Don't get what we expect when all examples are internally ground
-(run 1 (e1 e2)
-  (eval-programo
-    `(run* (z)
-       (letrec-rel ((five (f)
-                      (== ,e1 ,e2)))
-          (call-rel five 5)))
-    '((_.))))
+(test
+  (run 1 (e1 e2)
+    (eval-programo
+     `(run* (z)
+        (letrec-rel ((five (f)
+                           (== ,e1 ,e2)))
+                    (call-rel five 5)))
+     '((_.))))
+  '(((_.0 _.0) $$ (num _.0))))
 
-; Aha!
-(run 1 (x)
-  (eval-programo
-    `(run* (z)
-       (letrec-rel ((five (f)
-                      (== 7 7)))
-          (call-rel five 5)))
-    x))
+;; Aha!
+(test
+  (run 1 (x)
+    (eval-programo
+     `(run* (z)
+        (letrec-rel ((five (f)
+                           (== 7 7)))
+                    (call-rel five 5)))
+     x))
+  '(((_.))))
 
-(run 3 (e1 e2)
-  (eval-programo
-    `(run* (z)
-       (letrec-rel ((five (f)
-                      (== ,e1 ,e2)))
-          (call-rel five 5)))
-    '((_.))))
+(test
+  (run 3 (e1 e2)
+    (eval-programo
+     `(run* (z)
+        (letrec-rel ((five (f)
+                           (== ,e1 ,e2)))
+                    (call-rel five 5)))
+     '((_.))))
+  '(((_.0 _.0) $$ (num _.0)) (() ()) (5 f)))
 
-(run 1 (e1 e2)
-  (eval-programo
-    `(run* (z)
-       (letrec-rel ((five (f)
-                      (== ,e1 ,e2)))
-          (call-rel five z)))
-    '(5)))
+(test
+  (run 1 (e1 e2)
+    (eval-programo
+     `(run* (z)
+        (letrec-rel ((five (f)
+                           (== ,e1 ,e2)))
+                    (call-rel five z)))
+     '(5)))
+  '((5 f)))
 
 ; External grounding, extra examples to avoid overfitting, and with symbolo to
 ; fasten queries
-(run 1 (x y w)
-  (symbolo x)
-  (symbolo y)
-  (symbolo w)
-  (eval-programo
-    `(run* (z)
-       (letrec-rel ((appendo (l1 l2 l)
-                      (disj
-                        (conj (== '() l1) (== l2 l))
-                        (fresh (a)
-                          (fresh (d)
-                            (fresh (l3)
-                              (conj (== (cons a d) l1)
-                                    (conj (== (cons a l3) l)
-                                          (delay (call-rel appendo ,x
-                                                                   ,y
-                                                                   ,w))))))))))
-          (conj (call-rel appendo '(cat dog) '() '(cat dog))
-                (conj (call-rel appendo '(apple) '(peach) '(apple peach))
-                      (call-rel appendo '(1 2) '(3 4) z)))))
-    '((1 2 3 4))))
+(test
+  (run 1 (x y w)
+    (symbolo x)
+    (symbolo y)
+    (symbolo w)
+    (eval-programo
+     `(run* (z)
+        (letrec-rel ((appendo (l1 l2 l)
+                              (disj
+                               (conj (== '() l1) (== l2 l))
+                               (fresh (a)
+                                 (fresh (d)
+                                   (fresh (l3)
+                                     (conj (== (cons a d) l1)
+                                           (conj (== (cons a l3) l)
+                                                 (delay (call-rel appendo ,x
+                                                                  ,y
+                                                                  ,w))))))))))
+                    (conj (call-rel appendo '(cat dog) '() '(cat dog))
+                          (conj (call-rel appendo '(apple) '(peach) '(apple peach))
+                                (call-rel appendo '(1 2) '(3 4) z)))))
+     '((1 2 3 4))))
+  '((d l2 l3)))
 
 ; Thanks for the example, @bollu!
-(run* (count)
-  (eval-programo
-    `(run ,count (z)
-       (disj (== z 1)
-             (== z 2)))
-    '(1 2)))
+(test
+  (run* (count)
+    (eval-programo
+     `(run ,count (z)
+        (disj (== z 1)
+              (== z 2)))
+     '(1 2)))
+  '(((())) (((_.0)) $$ (=/= ((_.0 ()))))))
 
-(run* (count answers)
-  (eval-programo `(run ,count (z)
-                    (disj (== z 1)
-                          (== z 2)))
-                 answers))
+(test
+  (run* (count answers)
+    (eval-programo `(run ,count (z)
+                      (disj (== z 1)
+                            (== z 2)))
+                   answers))
+  '((() ()) ((()) (1)) (((())) (1 2)) ((((_.0)) (1 2)) $$ (=/= ((_.0 ()))))))
