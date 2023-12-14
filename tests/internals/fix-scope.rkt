b58f5a4fd4ce4856ddaf9bcea8381bfe2a0279bf
Michael Ballantyne <michael.ballantyne@gmail.com>
2023-12-14 11:59:07 -0500

    remove old fix-scope tests

--- tests/internals/fix-scope.rkt
+++ tests/internals/fix-scope.rkt
@@ -4,32 +4,6 @@
          "../../test-check.rkt")
 
 (test
-    (fix-scope
-     '(fresh ()
-        (== 1 _.0)))
-  '(fresh (_.0)
-     (== 1 _.0)))
-
-(test
-    (fix-scope
-     '(fresh ()
-        (== 1 _.0)
-        (fresh ()
-          (== _.0 _.1))))
-  '(fresh (_.0)
-     (== 1 _.0)
-     (fresh (_.1)
-       (== _.0 _.1))))
-
-(test
-    (fix-scope1
-     '(fresh ()
-        (== 1 _.0)
-        (fresh ()
-          (== _.0 _.1))))
-  '((fresh (_.0) (== 1 _.0) (fresh (_.1 _.0) (== _.0 _.1))) ()))
-
-(test
     (let ((r
            (fix-scope1-syntax
             #`(fresh ()
