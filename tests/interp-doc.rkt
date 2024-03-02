9def5a79f37f95c510c850a993e332f2f1cd7268
Nada Amin <namin@seas.harvard.edu>
2024-03-02 16:37:19 -0500

    show cleaned up generated code

--- tests/interp-doc.rkt
+++ tests/interp-doc.rkt
@@ -39,9 +39,30 @@
         (evalo e2 env v2)
         (later (finish-apply v1 applyo v2 v)))))))
 
-(run 3 (x v) (staged (evalo `(or x #t) `((x . ,x)) v)))
+
+(run* (x v) (staged (evalo `(or #f x) `((x . ,x)) v)))
+(generated-code)
+#|
+(lambda (v x) (disj (== v x)))
+|#
+
+(run* (x v) (staged (evalo `(or x #t) `((x . ,x)) v)))
+(generated-code)
+#|
+(lambda (x v)
+  (disj (conj (=/= x #f) (== v x))
+        (conj (== x #f) (== v #t))))
+|#
 
 (run 3 (e v) (staged (evalo `(or (or #f ,e) #f) '() v)))
+(generated-code)
+#|
+(lambda (e v)
+  (fresh (e-res)
+    (disj (invoke-fallback evalo/1 e '() e-res))
+    (disj (conj (=/= v #f) (== e-res v))
+          (conj (== e-res #f) (== v #f)))))
+|#
 
 (run 3 (e v) (staged (evalo `((lambda (x) (or (or #f ,e) x)) #f) '() v)))
 
@@ -51,6 +72,19 @@
 
 (run 3 (e v) (staged (evalo `((lambda (x) (or ,e x)) #f) '()  v)))
 
-(run 3 (e v) (staged (evalo `((lambda (f) (or (f #t) (f #f))) (lambda (x) x)) '() v)))
+(run* (e v) (staged (evalo `((lambda (f) (or (f #t) (f #f))) (lambda (x) x)) '() v)))
 (generated-code)
-
+#|
+(lambda (v)
+  (fresh (lam1 lam2)
+    (== lam1 (apply-rep 'applyo '(f (or (f #t) (f #f)) ())
+                        (lambda (f out)
+                          (fresh (f-res)
+                            (finish-apply f applyo #t f-res)
+                            (disj (conj (=/= out '#f) (== f-res out))
+                                  (conj (== f-res '#f)
+                                        (finish-apply f applyo #f out)))))))
+    (== lam2 (apply-rep 'applyo '(x x ())
+                        (lambda (x out) (== x out))))
+    (finish-apply lam1 applyo (lam2 v))))
+|#
