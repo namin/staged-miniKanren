7ce1ef3817c3ec237d61c9e5369168b50713fe41
Michael Ballantyne <michael.ballantyne@gmail.com>
2024-04-22 10:59:56 -0400

    add unstaged replicate, more tests

--- tests/applications/replicate.rkt
+++ tests/applications/replicate.rkt
@@ -2,17 +2,67 @@
 
 (require "../../main.rkt" "../../test-check.rkt")
 
-(defrel-partial/staged (replicate rep [n] [l res])
+;;
+;; Original unstaged program
+;;
+
+(defrel (replicate n l res)
+  (conde
+    [(== l '())
+     (== res '())]
+    [(fresh (a d rec-res)
+       (== l (cons a d))
+       (cons-n n a rec-res res)
+       (replicate n d rec-res))]))
+
+(defrel (cons-n n v l res)
+  (conde
+    [(== n 'Z)
+     (== l res)]
+    [(fresh (n-1 rec-res)
+       (== n `(S ,n-1))
+       (== res (cons v rec-res))
+       (cons-n n-1 v l rec-res))]))
+
+(test
+ (run 1 (q)
+   (cons-n '(S (S (S Z))) 1 '(2 2 2 3 3 3) q))
+ '((1 1 1 2 2 2 3 3 3)))
+
+(test
+ (run 1 (q)
+   (replicate '(S (S (S Z))) '(1 2 3) q))
+ '((1 1 1 2 2 2 3 3 3)))
+
+(test
+ (run 3 (q)
+   (fresh (n-2)
+     (replicate `(S (S ,n-2)) '(1 2 3) q)))
+ '((1 1 2 2 3 3) (1 1 1 2 2 2 3 3 3) (1 1 1 1 2 2 2 2 3 3 3 3)))
+
+(test
+ (run 2 (n l)
+   (fresh (n-2)
+     (replicate n l '(1 1 1 2 2 2 3 3 3))))
+ '(((S (S (S Z))) (1 2 3)) ((S Z) (1 1 1 2 2 2 3 3 3))))
+
+
+;;
+;; Staged
+;;
+
+
+(defrel-partial/staged (replicate/staged rep [n] [l res])
   (gather
    (conde
      [(== l '())
       (== res '())]
-     [(fresh (a d res-rec)
+     [(fresh (a d rec-res)
         (== l (cons a d))
-        (cons-n n a res-rec res)
-        (later (finish-apply rep replicate d res-rec)))])))
+        (cons-n/staged n a rec-res res)
+        (later (finish-apply rep replicate/staged d rec-res)))])))
 
-(defrel/staged (cons-n n v l res)
+(defrel/staged (cons-n/staged n v l res)
   (fallback
    (conde
      [(== n 'Z)
@@ -20,22 +70,21 @@
      [(fresh (n-1 rec-res)
         (== n `(S ,n-1))
         (== res (cons v rec-res))
-        (cons-n n-1 v l rec-res))])))
+        (cons-n/staged n-1 v l rec-res))])))
 
 
 ;; Runtime-only test of the cons-n helper
 (test
  (run 1 (q)
-   (fresh (rep)
-     (cons-n '(S (S (S Z))) 1 '(2 2 2 3 3 3) q)))
+   (cons-n/staged '(S (S (S Z))) 1 '(2 2 2 3 3 3) q))
  '((1 1 1 2 2 2 3 3 3)))
 
 ;; Runtime-only test of replicate
 (test
  (run 1 (q)
    (fresh (rep)
-     (partial-apply rep replicate '(S (S (S Z))))
-     (finish-apply rep replicate '(1 2 3) q)))
+     (partial-apply rep replicate/staged '(S (S (S Z))))
+     (finish-apply rep replicate/staged '(1 2 3) q)))
  '((1 1 1 2 2 2 3 3 3)))
 
 ;; Basic staged
@@ -43,19 +92,28 @@
  (run 1 (q)
    (staged
     (fresh (rep)
-      (specialize-partial-apply rep replicate '(S (S (S Z))))
-      (later (finish-apply rep replicate '(1 2 3) q)))))
+      (specialize-partial-apply rep replicate/staged '(S (S (S Z))))
+      (later (finish-apply rep replicate/staged '(1 2 3) q)))))
  '((1 1 1 2 2 2 3 3 3)))
 
 ;; Partially-staged: will replicate each element at least twice, but perhaps more.
 ;; The generated code has two unfolded `cons`es.
-;; This relies on the fallback to terminate at staging-time.
+;; This relies on the fallback to terminate at staging-time; otherwise the take*
+;;   done by the gather would unfold cons-n forever.
 (test
  (run 3 (q)
    (staged
     (fresh (rep n)
-      (specialize-partial-apply rep replicate `(S (S ,n)))
-      (later (finish-apply rep replicate '(1 2 3) q)))))
+      (specialize-partial-apply rep replicate/staged `(S (S ,n)))
+      (later (finish-apply rep replicate/staged '(1 2 3) q)))))
  '((1 1 2 2 3 3) (1 1 1 2 2 2 3 3 3) (1 1 1 1 2 2 2 2 3 3 3 3)))
 
-(generated-code)
+;; We don't expect any specialization in this fully-backwards example,
+;; but it should still work.
+(test
+ (run 2 (n l)
+   (staged
+    (fresh (rep)
+      (specialize-partial-apply rep replicate/staged n)
+      (later (finish-apply rep replicate/staged l '(1 1 1 2 2 2 3 3 3))))))
+ '(((S (S (S Z))) (1 2 3)) ((S Z) (1 1 1 2 2 2 3 3 3))))
