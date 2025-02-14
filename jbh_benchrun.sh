327bf0783e2f0f959d8bbaff70d1a03089566713
Jason Hemann <jason.hemann@gmail.com>
2025-02-14 15:58:44 -0500

    Adding variants of the benchmarks for an in-paper table

---
+++ jbh_benchrun.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+if [ -f "../staged-mk-paper" ]; then
+    BENCH_DIR="../staged-mk-paper"
+else
+    mkdir -p bench-results
+    BENCH_DIR="./jbh_bench-results"
+fi
+
+# (require "applications/parsing-with-derivatives.rkt")
+# (require "applications/proof.rkt")
+# (require "applications/dl.rkt")
+# (require "applications/peano-fib.rkt")
+# (require "applications/grammars.rkt")
+racket tests/bench-rel.rkt | tee bench-log-ex.txt
+python3 benchread.py >$BENCH_DIR/bench_rel.tex
+cp bench-log-ex.txt bench-log-ex-rel.txt
+
+
+# These must be the ones from the document.
+racket tests/doc-bench.rkt | tee bench-log-ex.txt
+python3 benchread.py >$BENCH_DIR/bench_doc.tex
+cp bench-log-ex.txt bench-log-ex-doc.txt
+
+
+# These belong, among others, in the benchmark table for the paper.
+racket tests/applications/double-eval.rkt | tee bench-log-ex.txt
+python3 benchread.py >$BENCH_DIR/bench_double_eval.tex
+cp bench-log-ex.txt bench-log-ex-double-eval.txt
+
+# Too much awesome for this paper.
+# racket tests/applications/metaKanren.rkt | tee bench-log-ex.txt
+# python3 benchread.py >$BENCH_DIR/bench_metaKanren.tex
+# cp bench-log-ex.txt bench-log-ex-metaKanren.txt
---
+++ tests/applications/jbh_replicate.rkt
@@ -0,0 +1,257 @@
+#lang racket/base
+
+(require "../../all.rkt")
+
+;;
+;; Original unstaged program
+;;
+
+;; Replicate every element of `l` `n` times to produce `res`
+(defrel (replicate n l res)
+  (conde
+    [(== l '())
+     (== res '())]
+    [(fresh (a d rec-res)
+       (== l (cons a d))
+       (cons-n n a rec-res res)
+       (replicate n d rec-res))]))
+
+;; Add `n` copies of `v` to the front of `l` to produce `res`
+(defrel (cons-n n v l res)
+  (conde
+    [(== n 'Z)
+     (== l res)]
+    [(fresh (n-1 rec-res)
+       (== n `(S ,n-1))
+       (== res (cons v rec-res))
+       (cons-n n-1 v l rec-res))]))
+
+(record-bench 'unstaged 'cons-n)
+(time-test
+ (run 1 (q)
+   (cons-n '(S (S (S Z))) 1 '(2 2 2 3 3 3) q))
+ '((1 1 1 2 2 2 3 3 3)))
+
+(record-bench 'unstaged 'replicate 1)
+(time-test
+ (run 1 (q)
+   (replicate '(S (S (S Z))) '(1 2 3) q))
+ '((1 1 1 2 2 2 3 3 3)))
+
+(record-bench 'unstaged 'replicate 4)
+(time-test
+ (run 3 (q)
+   (fresh (n-2)
+     (replicate `(S (S ,n-2)) '(1 2 3) q)))
+ '((1 1 2 2 3 3) (1 1 1 2 2 2 3 3 3) (1 1 1 1 2 2 2 2 3 3 3 3)))
+
+(record-bench 'unstaged 'replicate 3)
+(time-test
+ (run 2 (n l)
+   (fresh (n-2)
+     (replicate n l '(1 1 1 2 2 2 3 3 3))))
+ '(((S (S (S Z))) (1 2 3)) ((S Z) (1 1 1 2 2 2 3 3 3))))
+
+
+;;
+;; Equivalent MetaOCaml code (also in replicate.ml)
+;;
+
+;; let rec replicate_staged (n : peano) =
+;;   .<let rec replicate_n l =
+;;      match l with
+;;      | [] -> []
+;;      | a :: d -> .~(cons_n_staged n .<a>. .<(replicate_n d)>.)
+;;    in
+;;    replicate_n>.
+;; 
+;; let rec cons_n_staged (n : peano) (v : 'a code) (l : 'a list code) =
+;;   match n with
+;;   | Z -> l
+;;   | S p -> .<.~v :: .~(cons_n_staged p v l)>.
+
+
+;;
+;; Staged
+;;
+
+
+(defrel-partial/staged (replicate/staged rep [n] [l res])
+  (gather
+   (conde
+     [(== l '())
+      (== res '())]
+     [(fresh (a d rec-res)
+        (== l (cons a d))
+        (cons-n/staged n a rec-res res)
+        (later (finish-apply rep replicate/staged d rec-res)))])))
+
+(defrel/staged (cons-n/staged n v l res)
+  (fallback
+   (conde
+     [(== n 'Z)
+      (== l res)]
+     [(fresh (n-1 rec-res)
+        (== n `(S ,n-1))
+        (== res (cons v rec-res))
+        (cons-n/staged n-1 v l rec-res))])))
+
+
+;; Runtime-only test of the cons-n helper
+(record-bench 'staged 'cons-n)
+(time-test
+ (run 1 (q)
+   (cons-n/staged '(S (S (S Z))) 1 '(2 2 2 3 3 3) q))
+ '((1 1 1 2 2 2 3 3 3)))
+
+;; Runtime-only test of replicate
+(record-bench 'unstaged 'replicate 1)
+(time-test
+ (run 1 (q)
+   (fresh (rep)
+     (partial-apply rep replicate/staged '(S (S (S Z))))
+     (finish-apply rep replicate/staged '(1 2 3) q)))
+ '((1 1 1 2 2 2 3 3 3)))
+
+(record-bench 'staging 'replicate 1)
+(defrel (replicate/staged-basic-staged q)
+  (time-staged
+    (fresh (rep)
+      (specialize-partial-apply rep replicate/staged '(S (S (S Z))))
+      (later (finish-apply rep replicate/staged '(1 2 3) q)))))
+
+(record-bench 'staged 'replicate 1)
+;; Basic staged
+(time-test
+ (run 1 (q)
+   (replicate/staged-basic-staged q))
+ '((1 1 1 2 2 2 3 3 3)))
+
+;; Unstaged, running backwards in runtime portion
+(record-bench 'unstaged 'replicate 2)
+(time-test
+ (run 1 (q)
+   (replicate '(S (S (S Z))) q '(1 1 1 2 2 2 3 3 3)))
+ '((1 2 3)))
+
+(record-bench 'staging 'replicate 2)
+(defrel (replicated/staged-backwards-runtime q)
+  (time-staged
+    (fresh (rep)
+      (specialize-partial-apply rep replicate/staged '(S (S (S Z))))
+      (later (finish-apply rep replicate/staged q '(1 1 1 2 2 2 3 3 3))))))
+
+;; Staged, running backwards in runtime portion
+(record-bench 'staged 'replicate 2)
+(time-test
+ (run 1 (q)
+   (replicated/staged-backwards-runtime q))
+ '((1 2 3)))
+
+(record-bench 'staging 'replicate 4)
+(defrel (replicate/staged-partially-staged q)
+  (time-staged
+    (fresh (rep n)
+      (specialize-partial-apply rep replicate/staged `(S (S ,n)))
+      (later (finish-apply rep replicate/staged '(1 2 3) q)))))
+
+;; Partially-staged: will replicate each element at least twice, but perhaps more.
+;; The generated code has two unfolded `cons`es.
+;; This relies on the fallback to terminate at staging-time; otherwise the take*
+;;   done by the gather would unfold cons-n forever.
+(record-bench 'staged 'replicate 4)
+(time-test
+ (run 3 (q)
+   (replicate/staged-partially-staged q))
+ '((1 1 2 2 3 3) (1 1 1 2 2 2 3 3 3) (1 1 1 1 2 2 2 2 3 3 3 3)))
+
+(record-bench 'staging' replicate 3)
+(defrel (replicate/staged-no-specialization n l)
+  (time-staged
+    (fresh (rep)
+      (specialize-partial-apply rep replicate/staged n)
+      (later (finish-apply rep replicate/staged l '(1 1 1 2 2 2 3 3 3))))))
+
+;; We don't expect any specialization in this fully-backwards example,
+;; but it should still work.
+(record-bench 'staged 'replicate 3)
+(time-test
+ (run 2 (n l)
+   (replicate/staged-no-specialization n l))
+ '(((S (S (S Z))) (1 2 3)) ((S Z) (1 1 1 2 2 2 3 3 3))))
+
+;;
+;; As a single relation
+;;
+
+
+(defrel-partial/staged (replicate/staged-single rep [n] [l res])
+  (replicate-helper `(replicate ,rep ,n ,l ,res)))
+
+(defrel/staged (replicate-helper e)
+  (conde
+    [(fresh (n l res rep)
+       (== `(replicate ,rep ,n ,l ,res) e)
+       (gather
+        (conde
+          [(== l '())
+           (== res '())]
+          [(fresh (a d rec-res)
+             (== l (cons a d))
+             (replicate-helper `(cons-n ,n ,a ,rec-res ,res))
+             (later (finish-apply rep replicate/staged-single d rec-res)))])))]
+    [(fresh (n v l res)
+       (== `(cons-n ,n ,v ,l ,res) e)
+       (fallback
+        (conde
+          [(== n 'Z)
+           (== l res)]
+          [(fresh (n-1 rec-res)
+             (== n `(S ,n-1))
+             (== res (cons v rec-res))
+             (replicate-helper `(cons-n ,n-1 ,v ,l ,rec-res)))])))]))
+
+(record-bench 'staging 'replicate-single)
+(defrel (test-replicate/staged-single q)
+  (time-staged
+    (fresh (rep)
+      (specialize-partial-apply rep replicate/staged-single '(S (S (S Z))))
+      (later (finish-apply rep replicate/staged-single '(1 2 3) q)))))
+
+(record-bench 'staged 'replicate-single)
+(time-test
+ (run 1 (q) (test-replicate/staged-single q))
+ '((1 1 1 2 2 2 3 3 3)))
+
+
+;;
+;; Staged for a different mode: where we know `l` but not `n`.
+;;
+
+;; Note the asymmetry, due to the fact that cons-n doesn't call replicate.
+
+(defrel/staged (replicate/staged2 l n res)
+  (fallback
+   (conde
+     [(== l '())
+      (== res '())]
+     [(fresh (a d rec-res)
+        (== l (cons a d))
+        (later (cons-n n a rec-res res))
+        (replicate/staged2 d n rec-res))])))
+
+;; We can't / don't know how to write a replicate/staged that will specialize in both modes.
+(record-bench 'staging 'replicate2)
+(defrel (test-replicate/staged2 n q)
+  (time-staged
+   (replicate/staged2 '(1 2 3) n q)))
+
+(record-bench 'staged 'replicate2)
+(time-test
+ (run 1 (q)
+   (fresh (n)
+     (== n '(S (S (S Z))))
+	 (test-replicate/staged2 n q)))
+   '((1 1 1 2 2 2 3 3 3)))
+
+#;(generated-code)
