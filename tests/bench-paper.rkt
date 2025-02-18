d329abf1187ce8e3c1bce1d539e95c33f41b70fb
Jason Hemann <jason.hemann@gmail.com>
2025-02-18 09:42:22 -0500

    jbh_map and adding to bench-paper

--- tests/bench-paper.rkt
+++ tests/bench-paper.rkt
@@ -1,13 +1,13 @@
 #lang racket
 
 ;; Staging simple relations (vs unstaged)
-(require "applications/replicate.rkt") ;; Done
-(require "applications/power.rkt")     ;; Done
+(require "applications/jbh_replicate.rkt") ;; Done
+(require "applications/jbh_power.rkt")     ;; Done
 
 
 ;; Staging the evalo relation: write append as a function, compile as relation (thus faster flexible functions)
 ;;
-(require "synth-task-macros.rkt") ;; just the running backwards ;; Done
+(require "jbh_synth-task-macros-backwards.rkt") ;; just the running backwards ;; Done
 (require "interpreter/basics.rkt") ;; get append examples
 ;; --(require "../small-interp/staged.rkt")
 ;; -- (require "../small-interp/unstaged.rkt")
@@ -19,10 +19,10 @@
 ;; Staging the evalo relation running on (eval <program-text>) (thus faster eval functions in particular)
 (require "applications/or-lang-interp.rkt")
 (require "applications/proof.rkt")
-(require "applications/parsing-with-derivatives.rkt") ;; Done, already there
-(require "applications/double-eval.rkt") ;; quines w/quasiquotes ;; Done, already there.
+(require "applications/jbh_parsing-with-derivatives.rkt") ;; Done, already there
+(require "applications/jbh_double-eval.rkt") ;; quines w/quasiquotes ;; Done, already there.
 
 ;; Synthesis w/ground context
 ;; When we rely on a built-in primitive implemented in the object 'eval', you get a neat multi-level compiled-interpreted interaction
-(require "applications/map.rkt") ;; the example we wrote just for the purpose
-(require "synth-task-macros.rkt") ;; synthesize part of append from context ;; Done
+(require "applications/jbh_map.rkt") ;; the example we wrote just for the purpose
+(require "jbh_synth-task-macros-synth-context.rkt") ;; synthesize part of append from context ;; Done
